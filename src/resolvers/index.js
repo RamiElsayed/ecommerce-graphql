b37602a693975015eeb1ffc49efbff8309870afa
Rami Elsayed <ramielsayed123@outlook.com>
2022-09-04 17:40:15 +0100

    update product

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,6 +1,7 @@
 const products = require("./products");
 const product = require("./product");
 const createProduct = require("./createProduct");
+const updateProduct = require("./updateProduct");
 const resolvers = {
   Query: {
     products,
